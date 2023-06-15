b4dcc1acc0828b6d9f28893cc8a6c235644949af
MarinsLucas <lucasmarinsramalho@gmail.com>
2023-06-15 16:54:41 -0300

    código para testar incialização e posicionamento

--- cambio_borboleta/cambio_borboleta.ino
+++ cambio_borboleta/cambio_borboleta.ino
@@ -1,16 +1,20 @@
 #include <AccelStepper.h>
 #include "Wire.h"
 #define MAX_SPEED 5000 //velocidade máxima do nema17: 5000rpm
-#define GEAR_OFFSET 20 //20 passos = 36º (teoricamente)
 
 AccelStepper stepper(2, 10,11); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
-short upGearButton = 2; //declarando entradas de botões
+short upGearButton = 5; //declarando entradas de botões
 short downGearButton = 4;
+
+short maxPositionButton = 13; 
+short minPositionButton = 12; 
 bool changingGear;
 
 short gear = 0; //Marcha atual: inicialmente em 0 (neutro)
 
 long initialPos;
+
+long elapsedTime; 
 
 void writeGear(short gear)
 {
@@ -31,41 +35,102 @@ void setup()
   stepper.setMaxSpeed(MAX_SPEED);
   stepper.setSpeed(MAX_SPEED);
   //stepper.setAcceleration(10000.0); //TODO: qual aceleração colocar?
+  pinMode(LED_BUILTIN, OUTPUT); 
 
-  initialPos = stepper.currentPosition(); 
+  //Led acende para mostrar a inicialização
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  //FASE 1 DA INCIALIZAÇÃO: encontrar a posição central do fuso
+  initialPos = getInitialPos(); 
+
+  //FASE 2: mover o cambio para a posição inicial
+  stepper.runToNewPosition(initialPos);
+  delay(2);
+  stepper.setCurrentPosition(0);  
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  //Led apaga para mostrar que o sistema começou a funcionar
+    
+}
+
+long getInitialPos()
+{
+  long maxPosition;   
+  //Colocar o motor para se mover no sentindo negativo
+  stepper.setSpeed(-MAX_SPEED);        
+
+  //Motor continua se movendo até apertar o botão
+  while(digitalRead(minPositionButton) == LOW)
+  {
+    stepper.runSpeed();
+  }  
+  //define a posição 0, como a posição minima
+  stepper.setCurrentPosition(0);
+
+  //Coloca o motor para se mover no sentido positivo
+  stepper.setSpeed(MAX_SPEED);
+
+  while(digitalRead(maxPositionButton) == LOW)
+  {
+    stepper.runSpeed();
+  } 
+  maxPosition = stepper.currentPosition();
+
+  return maxPosition/2; 
+}
+
+void changeGear(int direction)
+{
+  //1 para direção positiva
+  if(direction == 1)
+  {
+    stepper.setSpeed(MAX_SPEED);
+    while(digitalRead(maxPositionButton) ==LOW)
+    {
+      stepper.runSpeed();       
+    }
+    stepper.runToNewPosition(0);
+  }     
+
+  //-1 para direção negativa
+  if(direction == -1)  
+  {
+    stepper.setSpeed(-MAX_SPEED);
+    while(digitalRead(minPositionButton) == HIGH)
+    {
+      stepper.runSpeed(); 
+    }    
+    stepper.runToNewPosition(0);
+  }
 }
 
 void loop()
 {
   //UP GEAR
-  if(digitalRead(upGearButton) == HIGH && gear < 5)
+  if(digitalRead(upGearButton) == HIGH)
   {
-    //movimenta a marcha pra trás e volta pro centro (tá certo isso)?
-    if(gear == 0)
-    {
-      stepper.runToNewPosition(-GEAR_OFFSET);
-      stepper.runToNewPosition(initialPos);
-    }
-    else //se a moto não estiver no neutro: faz o oposto
-    {
-      stepper.runToNewPosition(GEAR_OFFSET);
-      stepper.runToNewPosition(initialPos);
-    }
-    
+    elapsedTime = millis();
+    changeGear(1);
     Serial.println("Up Gear");    
     gear++;
+    writeGear(gear);
+    elapsedTime = millis() - elapsedTime;
+    Serial.println("Troca de marcha em: " + (String)elapsedTime + "ms");     
   }
 
   //DONW GEAR
   if(digitalRead(downGearButton) == HIGH && gear > 0)
   {
-    stepper.runToNewPosition(GEAR_OFFSET);
-    stepper.runToNewPosition(initialPos);
-
+    elapsedTime = millis();
+    changeGear(-1);
     Serial.println("Down Gear");  
     gear--;
+    writeGear(gear);
+    elapsedTime = millis() - elapsedTime;
+    Serial.println("Troca de marcha em: " + (String) elapsedTime + "ms");     
   }
 
-  writeGear(gear);
+  //Comentei por enquanto, pra não ficar spamando 
+  //writeGear(gear);
 
 }
